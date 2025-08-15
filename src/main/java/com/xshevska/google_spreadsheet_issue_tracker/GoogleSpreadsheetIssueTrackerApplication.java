8ac48baaa6c70bb171ac4df996b3a709c281afdb
miroslava <miroslavashevskaya12@gmail.com>
2025-08-15 19:12:22 +0200

    initial commit

---
+++ src/main/java/com/xshevska/google_spreadsheet_issue_tracker/GoogleSpreadsheetIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.xshevska.google_spreadsheet_issue_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSpreadsheetIssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSpreadsheetIssueTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xshevska/google_spreadsheet_issue_tracker/GoogleSpreadsheetIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xshevska.google_spreadsheet_issue_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSpreadsheetIssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
